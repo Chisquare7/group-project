4f16edd761cca3a8d4fc74099472bc8bebad3299
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-12-05 10:41:41 +0100

    feat: implement the checkMail file and its styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PressPage from "../src/pages/PressPage";
-import PressArticle from "../src/pages/PressArticle";
+// import PressPage from "../src/pages/PressPage";
+// import PressArticle from "../src/pages/PressArticle";
+import CheckMail from "../src/pages/CheckMail";
 
 function App() {
   return (
 		<div className="App">
 			<Router>
 				<Routes>
-					<Route path="/" element={<PressPage />} />
-					<Route path="/PressArticle" element={<PressArticle />} />
+					{/* <Route path="/" element={<PressPage />} />
+					<Route path="/PressArticle" element={<PressArticle />} /> */}
+					<Route path="/checkmail" element={<CheckMail />} />
 				</Routes>
 			</Router>
 		</div>
